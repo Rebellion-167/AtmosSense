96590f5f0f6e786c2e9516a456ebc11fc6c4bf58
Barnik Chakraborty <barnikc79@gmail.com>
2026-03-02 19:54:59 +0530

    Added ESP32 Web Dashboard for Temperature and Humidity

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,79 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include <DHT.h>
+#include "Secrets.h"
 
 #define DHTPIN 4
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
+// -------- WIFI --------
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
+
+WiFiServer server(80);
+
+// =====================================================
 void setup() {
     Serial.begin(115200);
     dht.begin();
-    Serial.println("DHT11 Indoor Air Quality - Phase 1");
+
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi");
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("\nConnected!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    server.begin();
 }
 
+// =====================================================
 void loop() {
+    WiFiClient client = server.available();
+    if (!client) return;
+
+    Serial.println("New Client Connected");
+
+    while (client.connected() && !client.available()) {
+        delay(1);
+    }
+
+    String request = client.readStringUntil('\r');
+    Serial.println(request);
+    client.flush();
+
     float temperature = dht.readTemperature();
     float humidity = dht.readHumidity();
 
     if (isnan(temperature) || isnan(humidity)) {
-        Serial.println("Failed to read from DHT11!");
-        delay(2000);
-        return;
+        temperature = 0;
+        humidity = 0;
     }
 
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.print(" °C | Humidity: ");
-    Serial.print(humidity);
-    Serial.println(" %");
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-type:text/html");
+    client.println("Connection: close");
+    client.println();
+    client.println("<!DOCTYPE html>");
+    client.println("<html><body>");
+    client.println("<h1>AtmosSense</h1>");
+    client.print("<p>Temperature: ");
+    client.print(temperature);
+    client.println("&deg;C</p>");
+    client.print("<p>Humidity: ");
+    client.print(humidity);
+    client.println(" %</p>");
+    client.println("</body></html>");
+    client.println();
 
-    delay(2000);
+    delay(1);
+    client.stop();
+    Serial.println("Client Disconnected");
 }
